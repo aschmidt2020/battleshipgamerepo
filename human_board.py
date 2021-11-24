56bfd2aeabe15627aa2ddfe22e8729fd3db3bf0e
Audrey Schmidt <audsch@outlook.com>
2021-11-24 10:08:34 -0600

    made computer_board and human_board child class of GameBoard parent class

--- human_board.py
+++ human_board.py
@@ -1,68 +1,10 @@
 from ship import Ship
-
-class HumanBoard: #inherit game_board
+from game_board import GameBoard
+class HumanBoard(GameBoard): #inherit game_board
     def __init__(self):
-        self.board = [[]]
-        self.create_board()
-        print('\n')
-
-        
-        self.ships_list = []
-        self.create_ships()
-         
         self.ships_locations = []
+        super().__init__()
 
-    def create_board(self): #creates 20x20 game board
-        grid = [ ]
-        rows, columns = (20, 20)
-        
-        for row in range(rows + 1):
-            row = []
-            for column in range(columns + 1):
-                row.append('-- ')
-            grid.append(row)
-            
-        self.board = grid
-    
-    def display_board(self): #displays board with 00-20 on rows and columns
-        rows, columns = (20, 20)
-        
-        for row in range(rows + 1):
-            if 0 <= row <= 9:
-                print(f'0{row}', '', end = ' ')
-            else:
-                print(row, '', end = ' ')
-                
-            for column in range(columns + 1):
-                if row == 0:
-                    if 0 <= column <= 9:
-                        print(f'0{column}', ' ', end = '')
-                    else:
-                        print(column, '', end = ' ')
-                else:
-                    print(self.board[row][column], end = ' ')
-            print()
-                
-    def create_ships(self): #instantiates 5 ships
-        destroyer = Ship(2, 'Destroyer') 
-        submarine = Ship(3, 'Sub')
-        battleship_one = Ship(4, 'Battleship 1')
-        battleship_two = Ship(4, 'Battleship 2')
-        aircraft = Ship(5, 'Aircraft Carrier')
-        
-        self.ships_list.append(destroyer)
-        self.ships_list.append(submarine)
-        self.ships_list.append(battleship_one)
-        self.ships_list.append(battleship_two)
-        self.ships_list.append(aircraft)
-    
-    def display_ships_to_place(self): #displays 5 ships and 
-        print('\nShips to place:')
-        for ship in self.ships_list:
-            print(f'{ship.name}, Size: {ship.size}')
-        print('\n')
-    
-    
     def can_place_ship(self, ship, direction, row, column):
         can_place_ship = False
         ship_size = ship.size
